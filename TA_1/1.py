a9e12007ef32fa722ce271754b6f31e22574b9a2
charlesspua <charlespuasdf@gmail.com>
2025-02-02 17:36:37 +0800

    yes

--- TA_1/1.py
+++ TA_1/1.py
@@ -1,17 +1,16 @@
-def count_characters(s):
-    vowels = "aeiouAEIOU"
-    v, c, sp, o = 0, 0, 0, 0
-    
-    for char in s:
-        if char in vowels:
-            v += 1
-        elif char.isalpha():
-            c += 1
-        elif char.isspace():
-            sp += 1
-        else:
-            o += 1
-    
-    print(f"Vowels: {v}, Consonants: {c}, Spaces: {sp}, Others: {o}")
 
-count_characters(input("Enter a string: "))
+stringg = input("Enter a string: ")
+vowels = "aeiouAEIOU"
+v, c, sp, o = 0, 0, 0, 0
+
+for char in stringg:
+    if char in vowels:
+        v += 1
+    elif char.isalpha():
+        c += 1
+    elif char.isspace():
+        sp += 1
+    else:
+        o += 1
+
+print(f"Vowels: {v}, Consonants: {c}, Spaces: {sp}, Others: {o}")
