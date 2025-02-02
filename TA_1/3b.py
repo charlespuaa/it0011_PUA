cf9541bdcbd526998f129c85a243bad8282417cb
charlesspua <charlespuasdf@gmail.com>
2025-02-02 14:10:38 +0800

    3b correected 666666

--- TA_1/3b.py
+++ TA_1/3b.py
@@ -1,5 +1,7 @@
-i = 1
+i = 1  # Start from 1
 while i <= 7:
-    if i % 2:
-        print(str(i) * i)
-    i += 1
+    if i % 2 == 1:  # print odd (1, 3, 5, 7)
+        print(" " * 4 + str(i) * i)  # spaces alignment 
+    if i == 5:  # 666666 
+        print(" " * 4 + "6" * 6)
+    i += 2  
