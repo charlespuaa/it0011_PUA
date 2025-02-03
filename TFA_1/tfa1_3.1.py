a91584d515b5d74c48ad4db0c7f9f79221d6a08f
charlesspua <charlespuasdf@gmail.com>
2025-02-03 10:30:50 +0800

    done

---
+++ TFA_1/tfa1_3.1.py
@@ -0,0 +1,12 @@
+# 3.1 String Manip 
+# Pua, Charles
+
+fName = input("Enter first name: ")
+lName = input("Enter last name: ")
+age = input("Enter age: ")
+
+fName, lName, age
+
+print("Full Name: ", fName + " " + lName)
+print("Sliced Name: ", fName[0:3])
+print("Hello, ", fName[0:3], "! Welcome. You are", age, "years old.")
---
+++ TFA_1/tfa1_3.2.py
@@ -0,0 +1,11 @@
+# 3.2 String Manip 
+# Pua, Charles
+
+fName = input("Enter first name: ")
+lName = input("Enter last name: ")
+fullName = fName + " " + lName
+
+print("Full Name:", fullName)
+print("Full Name (Upper Case):", (fullName.upper()) )
+print("Full Name (Lower Case):", (fullName.lower()) )
+print("Length of Full Name:", len(fullName) )
