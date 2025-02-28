b25f3f04e8b04ea0e9e94fdf17403f2acd1c9880
charlesspua <charlespuasdf@gmail.com>
2025-02-28 22:18:26 +0800

    partial wala pang C

---
+++ ACTIVITY_4/Set.py
@@ -0,0 +1,15 @@
+# PUA, Charles Michael G.
+# TW23
+
+A = {"a", "b", "c", "d", "f", "g"}
+B = {"b", "c", "l", "m", "o", "h"}
+C = {"c", "d","f", "h", "i", "j", "k"}
+
+# a. How many elements are there in set A and B
+totalAB = A.union(B)
+print(len(totalAB))
+
+# b. How many elements are there in B that is not part of A and C
+diffB = B.difference(A, C)
+print(len(diffB))
+
